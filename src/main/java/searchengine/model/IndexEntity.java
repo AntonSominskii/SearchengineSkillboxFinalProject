b5c6598b04733dc920d7a19b970ef7bca36b08ad
AntonSominskii <101739742+AntonSominskii@users.noreply.github.com>
2024-01-15 20:26:59 +0300

    Configuration class - MorphologyConfigurator added. Entities added. Repositories added.

---
+++ src/main/java/searchengine/model/IndexEntity.java
@@ -0,0 +1,38 @@
+package searchengine.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "search_index")
+public class IndexEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "page_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private PageEntity page;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "lemma_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private LemmaEntity lemma;
+
+    @NonNull
+    @Column(name = "lemma_rank", nullable = false)
+    private float lemmaRank;
+}
---
+++ src/main/java/searchengine/model/LemmaEntity.java
@@ -0,0 +1,40 @@
+package searchengine.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "Lemmas")
+public class LemmaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String lemma;
+
+    @NonNull
+    @Column(nullable = false)
+    private int frequency;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "site_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private SiteEntity site;
+
+    @OneToMany(mappedBy = "lemma", cascade = CascadeType.ALL)
+    private List<IndexEntity> indexEntities;
+}
