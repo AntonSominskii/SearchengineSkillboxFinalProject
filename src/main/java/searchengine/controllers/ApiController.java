2e2c12c58c7464b41f9cca4864eead318e83f089
AntonSominskii <101739742+AntonSominskii@users.noreply.github.com>
2024-03-24 15:50:50 +0300

    logback-spring.xml modified - now logs divided; in ApiController more logs added

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,8 +1,7 @@
 package searchengine.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import searchengine.dto.query.ActionResponse;
@@ -11,29 +10,42 @@ import searchengine.dto.query.StatisticsResponse;
 import searchengine.services.interfaces.IndexingService;
 import searchengine.services.interfaces.SearchService;
 import searchengine.services.interfaces.StatisticsService;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Log4j2
 public class ApiController {
-    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
     private final StatisticsService statisticsService;
     private final IndexingService indexingService;
     private final SearchService searchService;
 
     @GetMapping("/statistics")
     public ResponseEntity<StatisticsResponse> statistics() {
-        return ResponseEntity.ok(statisticsService.getStatistics());
+        try {
+            return ResponseEntity.ok(statisticsService.getStatistics());
+        } catch (Exception e) {
+            log.error("Error when getting statistics", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/startIndexing")
     public ResponseEntity<ActionResponse> startIndexing() {
-        return indexingService.startIndexing();
+        log.info("Indexing request received");
+        ResponseEntity<ActionResponse> response = indexingService.startIndexing();
+        log.info("Indexing started: {}", response.getBody());
+        return response;
     }
 
     @GetMapping("/stopIndexing")
     public ResponseEntity<ActionResponse> stopIndexing() {
-        return indexingService.stopIndexing();
+        log.info("Stop indexing request received");
+        ResponseEntity<ActionResponse> response = indexingService.stopIndexing();
+        log.info("Indexing stopped: {}", response.getBody());
+        return response;
     }
 
     @PostMapping("/indexPage")
@@ -48,9 +60,9 @@ public class ApiController {
             @RequestParam(name = "offset", required = false) Integer offset,
             @RequestParam(name = "limit", required = false) Integer limit
     ) {
-        logger.info("Search request received: query={}, site={}, offset={}, limit={}", query, site, offset, limit);
+        log.info("Search request received: query={}, site={}, offset={}, limit={}", query, site, offset, limit);
         ResponseEntity<QueryResponseData> response = searchService.search(query, site, offset, limit);
-        logger.info("Search response: {}", response);
+        log.info("Search response: {}", response);
         return response;
     }
 }
